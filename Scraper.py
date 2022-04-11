f124685be0b0aedad584b2ed0caf388ab87ce9e0
VilkaMini <alaunisvilius@gmail.com>
2022-04-11 19:13:14 +0300

    Fixed scraper for new website

--- Scraper.py
+++ Scraper.py
@@ -9,8 +9,7 @@ headers = {"User-Agent": "Mozilla/5.0"}
 url = "https://www.maxima.lt/akcijos"
 
 def main():
-    """Scrapes the maxima website for discounts.
-    """
+    """Scrapes the maxima website for discounts"""
     info = get_array()
     array_of_keys = list(info.keys())
 
@@ -18,38 +17,26 @@ def main():
     soup = BeautifulSoup(page.content, "html.parser")
 
     # Going though individual items
-    for item in soup.find_all("div", class_="offer-page-sale-item offer-page-sale-item__actual"):
+    for item in soup.find_all("div", class_="card-body offer-card d-flex flex-column justify-content-between"):
         # Creating a dictionary to put all item's info
         temp_info = get_array()
         # Taking image
-        img_div = item.find("div", class_="img")
-        temp_info["image_url"] = "https://www.maxima.lt" + str(img_div.find("img")["src"])
-        temp_info["item_name"] = item.find("div", class_="title").text
-        # Checking if description exists
-        if item.find("div", class_="desc"):
-            temp_info["description_ad"] = item.find("div", class_="desc").text
-        # Checking if price is bare
-        if item.find("div", class_="discount price bare"):
-            # Taking bare value
-            temp_info["eur_after_dc"] = item.find("span", class_="value").text
-            temp_info["cents_after_dc"] = item.find("span", class_="cents").text
-        # If price not bare
-        else:
-            # Checking if only discount is on display
-            if item.find("div", class_="discount percents"):
-                temp_info["discount_percent"] = item.find("span", class_="value").text
+        img_div = item.find("div", class_="offer-image w-100 d-flex justify-content-center position-relative")
+        temp_info["image_url"] = img_div.find("img")["data-src"]
+        temp_info["item_name"] = (item.find("h4", class_="mt-4 text-truncate text-truncate--2 align-self-end").text).replace('\n', '')
+        # Taking bare value
+        temp_info["eur_after_dc"] = (item.find("div", class_="price-eur").text).replace('\n', '')
+        temp_info["cents_after_dc"] = (item.find("span", class_="price-cents h-50").text).replace('\n', '')
+        # Checking if old price exists
+        if item.find("div", class_="price-old h-50 ms-1"):
+            # Discount banner
+            disc = item.find("div", class_="discount-icon")
+            if disc.find("img"):
+                temp_info["special_badge_url"] = disc.find("img")['src']
             else:
-                t1 = item.find("div", class_="t1")
-                temp_info["eur_after_dc"] = t1.find("span", class_="value").text
-                temp_info["cents_after_dc"] = t1.find("span", class_="cents").text
-                # Checking if extra banner exists
-                if item.find("div", class_="t1_1 t1_1_blue"):
-                    banner = item.find("div", class_="t1_1 t1_1_blue")
-                    temp_info["special_badge_url"] = "https://www.maxima.lt" + str(banner.find("img")["src"])
-                # Checking if price before is specified
-                if item.find("div", class_="t2"):
-                    t2 = item.find("div", class_="t2")
-                    temp_info["price_before_dc"] = t2.find("span", class_="value").text
+                temp_info["discount_percent"] = (disc.find("h3").text).replace('\n', '')
+            # Price before
+            temp_info["price_before_dc"] = (item.find("span", class_="text-decoration-line-through").text).replace('\n', '')
 
         info = copy_to_dict(array_of_keys, temp_info, info)
 
@@ -71,7 +58,6 @@ def get_array() -> dict:
         "cents_after_dc": [],
         "price_before_dc": [],
         "discount_percent": [],
-        "description_ad": [],
         "image_url": [],
         "special_badge_url": [],
     }
