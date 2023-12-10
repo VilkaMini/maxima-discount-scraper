de03c7d7d1053d1e1e16636e4a6a556a582e688a
VilkaMini <alaunisvilius@gmail.com>
2023-12-10 01:05:36 +0000

    Added documentation and preperation for postprocessing

--- scraperv2.py
+++ scraperv2.py
@@ -8,6 +8,7 @@ import pandas as pd
 from datetime import date, datetime
 
 class MaximaScraper:
+
     def __init__(self, date):
         self.date = date
         self.data = []
@@ -24,6 +25,10 @@ class MaximaScraper:
         self.options.add_argument("--disable-infobars")
 
     def get_driver(self):
+        """Gets driver for website.
+
+        :return: driver (webdriver) object with preloaded maxima discount website.
+        """
         driver = webdriver.Chrome(options=self.options)
         driver.get("https://www.maxima.lt/akcijos")
 
@@ -35,15 +40,30 @@ class MaximaScraper:
 
         return driver
 
-    def upload(self, data):
+    def upload(self, df):
+        """Saves data to csv file.
+
+        :param df: df (pd.DataFrame).
+        """
+        df.to_csv(f"maxima-discounts/{self.date}_maxima-discounts.csv", sep=';')
+
+    def postprocessing(self, data):
+        """Applies postprocessing to data.
+
+        :param data: array with dictionaries with discount data.
+        :return: df (pd.DataFrame) with processed values.
+        """
         df = pd.DataFrame(data)
-        df.to_csv(f"{self.date}_maxima-discounts.csv")
+        df["Image_link"] = pd.concat([df["Image_link"], df["Image_link_visible"]])
+
+        df.drop(columns=["Image_link_visible"], inplace=True)
+
+        return df
 
     def scrape_maxima(self, driver):
         """Scrapes Maxima Discount page with all the information in it.
 
         :param driver: Webdriver with open Maxima discount page.
-        :return: None
         """
         for category in driver.find_elements(By.CLASS_NAME, value="my-5"):
             category_name = category.find_element(By.TAG_NAME, value="h2").get_attribute("textContent")
@@ -52,42 +72,44 @@ class MaximaScraper:
             for discount_item in category.find_elements(By.CLASS_NAME, value="card-body"):
                 discount_data = {}
 
-                discount_data['category'] = category_name
+                discount_data['Category'] = category_name
 
-                discount_data['image_link_visible'] = discount_item.find_element(By.TAG_NAME, value="img").get_attribute("src")
-                discount_data['image_link'] = discount_item.find_element(By.TAG_NAME, value="img").get_attribute("data-src")
+                discount_data['Image_link_visible'] = discount_item.find_element(By.TAG_NAME, value="img").get_attribute("src")
+                discount_data['Image_link'] = discount_item.find_element(By.TAG_NAME, value="img").get_attribute("data-src")
 
                 try:
-                    discount_data['discount_icon_text'] = discount_item.find_element(By.TAG_NAME, value="h3").get_attribute("textContent")
+                    discount_data['Discount_icon_text'] = discount_item.find_element(By.TAG_NAME, value="h3").get_attribute("textContent")
                 except:
-                    discount_data['discount_icon_text'] = None
+                    discount_data['Discount_icon_text'] = None
 
-                discount_data['item_name'] = discount_item.find_element(By.TAG_NAME, value="h4").get_attribute("textContent")
-                discount_data['item_discount_time'] = discount_item.find_element(By.CLASS_NAME, value="offer-dateTo-wrapper").get_attribute("textContent")
+                discount_data['Item_name'] = discount_item.find_element(By.TAG_NAME, value="h4").get_attribute("textContent")
+                discount_data['Item_discount_time'] = discount_item.find_element(By.CLASS_NAME, value="offer-dateTo-wrapper").get_attribute("textContent")
                 try:
-                    discount_data['discount_shop_size'] = len(discount_item.find_elements(By.CLASS_NAME, value="x-icon"))
+                    discount_data['Discount_shop_size'] = len(discount_item.find_elements(By.CLASS_NAME, value="x-icon"))
                 except:
-                    discount_data['discount_shop_size'] = None
+                    discount_data['Discount_shop_size'] = None
 
-                discount_data['item_price_euro'] = discount_item.find_element(By.CLASS_NAME, value="price-eur").get_attribute("textContent")
-                discount_data['item_price_cents'] = discount_item.find_element(By.CLASS_NAME, value="price-cents").get_attribute("textContent")
+                discount_data['Item_price_euro'] = discount_item.find_element(By.CLASS_NAME, value="price-eur").get_attribute("textContent")
+                discount_data['Item_price_cents'] = discount_item.find_element(By.CLASS_NAME, value="price-cents").get_attribute("textContent")
                 try:
-                    discount_data['discount_text_decorator'] = discount_item.find_element(By.CLASS_NAME, value="text-decoration-line-through").get_attribute("textContent")
+                    discount_data['Discount_text_decorator'] = discount_item.find_element(By.CLASS_NAME, value="text-decoration-line-through").get_attribute("textContent")
                 except:
-                    discount_data['discount_text_decorator'] = None
+                    discount_data['Discount_text_decorator'] = None
 
                 try:
                     discount_facilitator = discount_item.find_element(By.CLASS_NAME, value="offer-bottom-icon-wrapper")
-                    discount_data['discount_facilitator'] = discount_facilitator.find_element(By.TAG_NAME, value="img").get_attribute("alt")
+                    discount_data['Discount_facilitator'] = discount_facilitator.find_element(By.TAG_NAME, value="img").get_attribute("alt")
                 except:
-                    discount_data['discount_facilitator'] = None
-
+                    discount_data['Discount_facilitator'] = None
                 self.data.append(discount_data)
 
     def execute_scraper(self):
+        """Executes the scraper logic.
+        """
         print("Starting Maxima Discount Scraper")
         self.scrape_maxima(self.get_driver())
-        self.upload(self.data)
+        df = self.postprocessing(self.data)
+        self.upload(df)
         print("Maxima Discount Scraper finished")
 
 
